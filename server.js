a9f07b791451c69a389ce272c456441f52e93409
outsung <insung9546@gmail.com>
2021-07-11 18:10:57 +0900

    init

--- server.js
+++ server.js
@@ -1,31 +1,36 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const socketIo = require('socket.io');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const socketIo = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
-const domains = ['http://localhost:3000', 'https://outsung.github.io'];
+const domains = [
+  "http://localhost:19000",
+  "http://localhost:3000",
+  "https://outsung.github.io",
+  // "https://amritb.github.io",
+];
 
 // cors
 const corsOptions = {
-  origin: function(origin, callback){
-  	const isTrue = domains.indexOf(origin) !== -1;
+  origin: function (origin, callback) {
+    const isTrue = domains.indexOf(origin) !== -1;
     callback(null, isTrue);
   },
-  credentials: true
-}
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
 // server start
 const server = app.listen(port, () => {
-  console.log('Server is running on port : ' + port);
+  console.log("Server is running on port : " + port);
 });
 
 // socket start
 const io = socketIo(server);
-require('./src/socket/socket')(io);
+require("./src/socket/socket")(io);
--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,10 +1,18 @@
+module.exports = (io) => {
+  // https://amritb.github.io/socketio-client-tool/
+  // console.log(io);
 
-module.exports = (io) => {    // https://amritb.github.io/socketio-client-tool/
-    io.on('connection', (socket) => {
-        socket.on('animationStart', () => {
-            socket.emit('animationStart');
-        });
+  io.on("connection", (socket) => {
+    console.log("connection!!!");
+
+    socket.on("animationStart", () => {
+      console.log("animationStart!!");
+      io.emit("animationStart");
     });
-}
 
-
+    socket.on("upIntensity", () => {
+      console.log("upIntensity!!");
+      io.emit("upIntensity");
+    });
+  });
+};
